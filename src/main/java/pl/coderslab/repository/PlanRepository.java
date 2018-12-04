d6dfcc42beb888cdd5aeee8084787fb477429f09
Teanka <arolicka@gmail.com>
2018-12-04 23:35:13 +0100

    added plan&recipe sql queries (new methods in repositories)

--- src/main/java/pl/coderslab/repository/PlanRepository.java
+++ src/main/java/pl/coderslab/repository/PlanRepository.java
@@ -1,8 +1,11 @@
 package pl.coderslab.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.model.Plan;
 
-public interface PlanRepository extends JpaRepository<Plan, Long>
-{
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+    @Query(value = "SELECT count(*) FROM plan p WHERE p.admin_id = ?1", nativeQuery = true)
+    int getNumberOfPlansEnteredByAdmin(int adminId);
+
 }
